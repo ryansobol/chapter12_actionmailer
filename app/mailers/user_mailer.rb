04ef1971df95acb57e5605b9306c363bf11b1887
Ryan Sobol <contact@ryansobol.com>
2012-02-21 11:13:02 -0800

    Add image attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
     @user = user
+    attachments["rails.png"] = File.read("#{Rails.root}/app/assets/images/rails.png")
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
   end
 end
