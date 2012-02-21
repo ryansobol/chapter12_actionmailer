fbb95d25909c28ae34bca931a00f1cd6bc5856d0
Ryan Sobol <contact@ryansobol.com>
2012-02-20 20:47:55 -0800

    Add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapter11Activemailer::Application.config.secret_token = '681d822113331371ea070f005d6f9c99e01e87cb751e1eda2b71bad616b9587cb9dd4d7462daff450b508270f0fbd7b4eaa00c6f49eb0925c69776292690b748'
