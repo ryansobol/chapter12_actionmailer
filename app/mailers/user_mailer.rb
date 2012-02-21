c8a8b76bebb227d3c07c0badde07460d5f1a0d2c
Ryan Sobol <contact@ryansobol.com>
2012-02-21 11:11:03 -0800

    Add multipart email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
     @user = user
-    mail(:to => user.email, :subject => "Registered")
+    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
   end
 end
